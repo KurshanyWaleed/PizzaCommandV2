8ec45f841876a5ef88943f4f344f02e795fecc01
waleed <kurshany.waleed@gmail.com>
2022-01-31 16:01:34 +0100

    panier functions are complited for mobile

--- lib/Presentation/pages/panierScreens/level1_screens/panier_liste.dart
+++ lib/Presentation/pages/panierScreens/level1_screens/panier_liste.dart
@@ -1,11 +1,8 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:j_pizza_v2/BolC/cubit/paniercounter_cubit.dart';
-
 import 'package:j_pizza_v2/BolC/cubitBasket/basket_state.dart';
 import 'package:j_pizza_v2/BolC/cubitBasket/basket_cubit.dart';
+import 'package:j_pizza_v2/Presentation/widgets/basket_item.dart';
 
 class PanieList extends StatelessWidget {
   const PanieList({Key? key}) : super(key: key);
@@ -13,7 +10,6 @@ class PanieList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<BasketCubit2, BasketStateV2>(builder: (context, state) {
-      print(BasketStateV2.basketList.isEmpty);
       return BasketStateV2.basketList.isEmpty
           ? Container(
               width: MediaQuery.of(context).size.width,
@@ -84,134 +80,5 @@ class PanieList extends StatelessWidget {
               ],
             );
     });
-  }
-}
-
-class BasketItem extends StatelessWidget {
-  const BasketItem({
-    Key? key,
-    required this.element,
-  }) : super(key: key);
-  final dynamic element;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-      width: MediaQuery.of(context).size.width,
-      height: 50,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8), color: Colors.redAccent),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Text(
-            element.value.toString() + "X",
-            style: const TextStyle(
-                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          Flexible(
-            child: Row(
-              children: [
-                Image(
-                  image: AssetImage(
-                    element.key.imagepath,
-                  ),
-                  width: 30,
-                  height: 30,
-                ),
-                const SizedBox(
-                  width: 30,
-                ),
-                Text(element.key.title,
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 10,
-                        fontWeight: FontWeight.bold)),
-              ],
-            ),
-          ),
-          BlocBuilder<PaniercounterCubit, int>(
-            builder: (context, state) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      BlocProvider.of<BasketCubit2>(context).onEncreaseQuantity(
-                          element.key,
-                          element.value <= 1 ? 1 : element.value + 1);
-                      // BlocProvider.of<PaniercounterCubit>(context).inc();
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(50)),
-                      child: const Icon(
-                        Icons.add,
-                        size: 30,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Center(
-                    child: Text(element.value.toString(),
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 30)),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      BlocProvider.of<BasketCubit2>(context).ondesreaseQuantity(
-                          element.key,
-                          element.value <= 1 ? 1 : element.value - 1);
-                      //  BlocProvider.of<PaniercounterCubit>(context).dnc();
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(50)),
-                      child: const Icon(
-                        Icons.remove,
-                        size: 30,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  InkWell(
-                    onTap: () => BlocProvider.of<BasketCubit2>(context)
-                        .onDeleteItem(element.key),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 5),
-                      height: 30,
-                      child: const Center(
-                          child: Text("Annuler",
-                              style: TextStyle(
-                                  color: Colors.red,
-                                  fontWeight: FontWeight.bold))),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  )
-                ],
-              );
-            },
-          )
-        ]),
-      ),
-    );
   }
 }
